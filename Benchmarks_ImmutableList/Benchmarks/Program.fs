d942301fbc775b3b715ea96d8e240457c2c4e918
jiv <jiv@ciklum.com>
2023-11-16 17:33:22 +0100

    lzgit - 2023-11-16T16:33:21.920Z

---
+++ Benchmarks_ImmutableList/Benchmarks/Program.fs
@@ -0,0 +1,165 @@
+﻿module ListBenchmarks
+
+open System
+open System.Linq
+open System.Collections.Immutable
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Running
+
+type MyRecord = { Id: int; Name: string }
+
+type DU =
+    | A of int
+    | B of string
+
+let inline test xs f =
+    for i in xs do
+        f i xs |> ignore
+
+let size = 1000
+let input<'T>() : 'T list =
+    let listOfInts = [ 1..size ]
+    let listOfStrings = listOfInts |> List.map (fun i -> i.ToString())
+    let listOfRecords =
+        listOfInts |> List.map (fun i -> { Id = i; Name = i.ToString() })
+
+    match typeof<'T> with
+    | t when t = typeof<int> -> listOfInts |> Seq.cast<'T> |> List.ofSeq
+    | t when t = typeof<string> -> listOfStrings |> Seq.cast<'T> |> List.ofSeq
+    | t when t = typeof<MyRecord> -> listOfRecords |> Seq.cast<'T> |> List.ofSeq
+
+[<GenericTypeArguments(typeof<int>)>]
+type GenericTests<'T>() =
+    let xs = input<'T>()
+
+    // [<Benchmark>]
+    // member _.ListRev() =
+    //     List.rev xs
+
+    [<Benchmark>]
+    member _.ListAddToEnd() =
+        let rec go i acc =
+            if i = 0 then acc
+            else go (i - 1) (acc @ [i])
+        go size []
+
+    [<Benchmark>]
+    member _.ListAddToEndAcc() =
+        let rec go i acc =
+            if i = 0 then acc
+            else go (i - 1) (i :: acc)
+        go size [] |> List.rev
+
+    [<Benchmark>]
+    member _.ListSumIndexed() =
+        let rec go i acc =
+            if i = 0 then acc
+            else go (i - 1) (acc + xs.[i])
+        go size [] |> List.rev
+
+[<MemoryDiagnoser>]
+type ListImmListTests() =
+    let size = 1000
+    let listOfInts = [ 1..size ]
+    let listOfIntsSmaller = [ 1..100 ]
+    let listOfStrings = listOfInts |> List.map (fun i -> i.ToString())
+    let listOfRecords =
+        listOfInts |> List.map (fun i -> { Id = i; Name = i.ToString() })
+
+    let immListOfInts = listOfInts |> ImmutableList.CreateRange
+    let immListOfIntsSmaller = listOfIntsSmaller |> ImmutableList.CreateRange
+
+    [<Benchmark(Description = "int - List cons")>]
+    member _.IntListCons() =
+        let rec go i acc =
+            if i = 0 then acc
+            else go (i - 1) (i :: acc)
+        go size []
+
+    [<Benchmark(Description = "int - ImmutableList cons")>]
+    member _.IntImmListCons() =
+        let rec go i (acc: ImmutableList<_>) =
+            if i = 0 then acc
+            else go (i - 1) (acc.Add i)
+        go size ImmutableList<int>.Empty
+
+    [<Benchmark(Description = "int - List.reverse")>]
+    member _.IntListReverse() =
+        listOfInts |> List.rev
+
+    [<Benchmark(Description = "int - ImmutableList.reverse")>]
+    member _.IntImmListReverse() =
+        immListOfInts.Reverse()
+
+    [<Benchmark(Description = "int - List.map")>]
+    member _.IntListMap() =
+        listOfInts |> List.map ((+) 1)
+
+    [<Benchmark(Description = "int - ImmutableList map by LINQ Select")>]
+    member _.IntImmListMap() =
+        immListOfInts.Select ((+) 1) |> ImmutableList.CreateRange
+
+    [<Benchmark(Description = "int - ImmutableList map by SetItem")>]
+    member _.IntImmListMapSetItem() =
+        let mutable xs = immListOfInts
+        for i=0 to size-1 do
+            xs <- immListOfInts.SetItem(i, i + 1)
+        xs
+
+    [<Benchmark(Description = "int - ImmutableList map by Builder")>]
+    member _.IntImmListMapBuilder() =
+        //let mutable xs = immListOfInts
+        let b = immListOfInts.ToBuilder()
+        for i=0 to size-1 do
+            b[i] <- i + 1
+        b.ToImmutable()
+
+    [<Benchmark(Description = "int - List.filter")>]
+    member _.IntListFilter() =
+        listOfInts |> List.filter (fun i -> i % 2 = 0)
+
+    [<Benchmark(Description = "int - ImmutableList filter by LINQ Where")>]
+    member _.IntImmListFilter() =
+        immListOfInts.Where (fun i -> i % 2 = 0) |> ImmutableList.CreateRange
+
+    [<Benchmark(Description = "int - ImmutableList filter by RemoveAll")>]
+    member _.IntImmListFilterRemoveAll() =
+        immListOfInts.RemoveAll (fun i -> i % 2 <> 0)
+
+    [<Benchmark(Description = "int - List.reduce")>]
+    member _.IntListReduce() =
+        listOfInts |> (List.reduce (+))
+
+    [<Benchmark(Description = "int - ImmutableList.reduce")>]
+    member _.IntImmListReduce() =
+        let mutable x = 0
+        immListOfInts.ForEach (fun y -> x <- x + y)
+
+    [<Benchmark(Description = "int - List.contains")>]
+    member _.IntListContains() =
+        test listOfIntsSmaller List.contains
+
+    [<Benchmark(Description = "int - ImmutableList.contains")>]
+    member _.IntImmListContains() =
+        test immListOfIntsSmaller (fun i xs -> xs.Contains i)
+
+    // [<Benchmark(Description = "string - List.exists")>]
+    // member _.StringListExists() =
+    //     test listOfStrings (fun i -> List.exists (fun item -> item = i))
+    //
+    // [<Benchmark(Description = "string - List.contains")>]
+    // member _.StringListContains() =
+    //     test listOfStrings List.contains
+    //
+    // [<Benchmark(Description = "record - List.exists")>]
+    // member _.RecordListExists() =
+    //     test listOfRecords (fun i -> List.exists (fun item -> item.Id = i.Id))
+    //
+    // [<Benchmark(Description = "record - List.contains")>]
+    // member _.RecordListContains() =
+    //     test listOfRecords (fun i -> List.contains i)
+
+//InProcessEmitToolchain.Instance
+//BenchmarkRunner.Run<ListImmListTests>()
+BenchmarkRunner.Run<GenericTests<int>>()
+//BenchmarkRunner.Run([| typeof<ListTests>; typeof<ArrayTests>; typeof<SeqTests> |])
--- ds.md
+++ ds.md
@@ -104,12 +104,43 @@ let listA2 = 1 :: 2 :: 3 :: []
 let listB = [4; 1; 2; 3]
 let listB2 = 4 :: listA
 
-listA = listA2, System.Object.ReferenceEquals(listA, listA2)
-listB = listB2, System.Object.ReferenceEquals(listB, listB2)
-System.Guid.Empty
+listA = listA2
+listB = listB2
+
 (*
 
 ---
+
+- fast iteration, mapping, filtering
+- slow indexing, append on end
+- `x :: xs` super fast
+- `xs @ ys` slow
+
+---
+*)
+
+    [<Benchmark>]
+    member _.ListAddToEnd() =
+        let rec go i acc =
+            if i = 0 then acc
+            else go (i - 1) (acc @ [i])
+        go size []
+
+    [<Benchmark>]
+    member _.ListAddToEndAcc() =
+        let rec go i acc =
+            if i = 0 then acc
+            else go (i - 1) (i :: acc)
+        go size [] |> List.rev
+
+(*
+
+|          Method |        Mean |      Error |     StdDev |
+|---------------- |------------:|-----------:|-----------:|
+|    ListAddToEnd | 5,178.36 us | 102.125 us | 139.790 us |
+| ListAddToEndAcc |    15.99 us |   0.308 us |   0.303 us |
+
+- List.rev is fast!
 
 ```mermaid
 graph TD;
